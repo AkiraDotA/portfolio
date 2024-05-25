cdffa3d0a26c18a5fbbf1dde677bb72667ec82f5
Zac Holly <zacholly@sbcglobal.net>
2024-05-24 23:41:27 -0500

    Add opacity and blur to mobile navigation style

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div class="flex lg:flex-row flex-col my-16 max-sm:mt-4 mx-8 max-sm:mx-3">
-    <TheMenu class="lg:max-h-[800px]" />
+    <TheMenu />
     <UCard class="w-full">
       <slot />
     </UCard>
