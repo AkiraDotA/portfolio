d1ac7809f5b0fddd78b7ccf2a98d0c1c83696719
Zac Holly <zacholly@sbcglobal.net>
2024-08-24 21:35:30 -0500

    Remove borders from navigation buttons

--- components/TheNavigation.vue
+++ components/TheNavigation.vue
@@ -1,12 +1,12 @@
 <script setup>
 const availableTints = [
-	'border-darkred bg-darkred',
-	'border-darkred-100 bg-darkred-100',
-	'border-darkred-200 bg-darkred-200',
-	'border-darkred-300 bg-darkred-300',
-	'border-darkred-400 bg-darkred-400',
-	'border-darkred-500 bg-darkred-500',
-	'border-darkred-600 bg-darkred-600',
+	'bg-darkred',
+	'bg-darkred-100',
+	'bg-darkred-200',
+	'bg-darkred-300',
+	'bg-darkred-400',
+	'bg-darkred-500',
+	'bg-darkred-600',
 ];
 </script>
 
@@ -18,13 +18,12 @@ const availableTints = [
         v-for="(navItem, index) of NAVIGATION"
         :key="index"
         :to="navItem.route"
-        class="lg:border-x-2 border-y-2"
-        :class="[availableTints[index + 1], {'border-s-2': index === 0}, {'border-e-2': index === NAVIGATION.length - 1}]"
+        :class="availableTints[index + 1]"
       >
         {{ navItem.label }}
       </NavigationButton>
     </nav>
-    <div class="nav-ender h-8 mb-6 max-lg:hidden" :class="availableTints[NAVIGATION.length + 1]" />
+    <div class="nav-ender h-8 mb-6 max-lg:hidden" :class="availableTints[NAVIGATION.length + 1] ?? 'bg-darkred-600'" />
   </div>
 </template>
 
