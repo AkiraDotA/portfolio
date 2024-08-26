ea0667c83a02eac145f342ef7411a31cf1caae00
Zac Holly <zacholly@sbcglobal.net>
2024-08-25 21:05:34 -0500

    Add "Contact" page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,25 @@
 
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/eslint-module', '@nuxt/ui', '@nuxtjs/device'],
+	modules: [
+		'@nuxtjs/eslint-module',
+		'@nuxt/ui',
+		'@nuxtjs/device',
+		['nuxt-mail', {
+			message: {
+				to: process.env.NUXT_MAIL_USERNAME,
+			},
+			smtp: {
+				host: process.env.NUXT_MAIL_SMPT,
+				port: process.env.NUXT_MAIL_PORT,
+				secure: true,
+				auth: {
+					user: process.env.NUXT_MAIL_USERNAME,
+					pass: process.env.NUXT_MAIL_PASSWORD,
+				},
+			},
+		}],
+	],
 	imports: {
 		presets: [
 			{
@@ -18,7 +36,10 @@ export default defineNuxtConfig({
 	app: {
 		head: {
 			link: [
-				{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap' },
+				{
+					rel: 'stylesheet',
+					href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap',
+				},
 			],
 		},
 		pageTransition: {
@@ -29,4 +50,5 @@ export default defineNuxtConfig({
 	ui: {
 		icons: ['mdi', 'simple-icons', 'hugeicons'],
 	},
+	compatibilityDate: '2024-08-25',
 });
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,9 +1,129 @@
 <script setup>
+const contactFormData = reactive({
+	name: null,
+	email: null,
+	subject: null,
+	message: null,
+});
+
+const clearForm = () => {
+	contactFormData.name = null;
+	contactFormData.email = null;
+	contactFormData.subject = null;
+	contactFormData.message = null;
+};
+
+const validateForm = () => {
+	const errors = [];
+	if (!contactFormData.name) {
+		errors.push({ path: 'name', message: 'Required' });
+	}
+
+	if (!contactFormData.email) {
+		errors.push({ path: 'email', message: 'Required' });
+	}
+
+	if (!contactFormData.message) {
+		errors.push({ path: 'message', message: 'Required' });
+	}
+
+	const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+	if (!emailRegex.test(contactFormData.email)) {
+		errors.push({ path: 'email', message: 'Invalid email' });
+	}
+
+	return errors;
+};
+
+const form = ref(null);
+const submitForm = async () => {
+	const mail = useMail();
+	mail.send({
+		from: `${contactFormData.name} [${contactFormData.email}] via Contact Form <${contactFormData.email}>`,
+		subject: contactFormData.subject || 'Website Contact',
+		text: contactFormData.message,
+	});
+
+	clearForm();
+};
 </script>
 
 <template>
   <div>
     <SectionTitle>Contact</SectionTitle>
-    Placeholder
+    <UForm
+      ref="form"
+      class="space-y-6"
+      :state="contactFormData"
+      :validate="validateForm"
+      :validate-on="['submit']"
+      @submit="submitForm"
+    >
+      <UFormGroup
+        label="Your Name"
+        name="name"
+        size="xl"
+        required
+      >
+        <UInput
+          v-model.trim="contactFormData.name"
+          placeholder="John Doe"
+          size="lg"
+          icon="i-mdi-account-outline"
+          required
+        />
+      </UFormGroup>
+      <UFormGroup
+        label="Your Email"
+        name="email"
+        size="xl"
+        required
+      >
+        <UInput
+          v-model.trim="contactFormData.email"
+          placeholder="johndoe@example.com"
+          size="lg"
+          type="email"
+          icon="i-mdi-email-outline"
+          required
+        />
+      </UFormGroup>
+      <UFormGroup
+        label="Subject"
+        name="subject"
+        size="xl"
+      >
+        <UInput
+          v-model.trim="contactFormData.subject"
+          placeholder="Contacting you for..."
+          size="lg"
+        />
+      </UFormGroup>
+      <UFormGroup
+        label="Message"
+        name="message"
+        size="xl"
+        required
+      >
+        <UTextarea
+          v-model.trim="contactFormData.message"
+          placeholder="Hello, I would like to discuss..."
+          size="lg"
+          :rows="4"
+          autoresize
+          :maxrows="8"
+          required
+        />
+      </UFormGroup>
+
+      <UButton
+        type="submit"
+        color="darkred"
+        size="lg"
+        class="!text-gray-300"
+      >
+        Submit
+      </UButton>
+    </UForm>
   </div>
 </template>
