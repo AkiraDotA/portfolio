b6a74fd1123db606ecd1e68ebcb273cfc0f420f3
Zac Holly <zacholly@sbcglobal.net>
2024-08-30 16:41:12 -0500

    Add custom error page

--- components/NavigationButton.vue
+++ components/NavigationButton.vue
@@ -4,6 +4,10 @@ const { to } = defineProps({
 		type: String,
 		required: true,
 	},
+	hideActive: {
+		type: Boolean,
+		default: false,
+	},
 });
 
 const route = useRoute();
@@ -15,7 +19,7 @@ const route = useRoute();
     class="group block hover:bg-gray-800 transition duration-300 lg:text-lg sm:text-sm max-sm:text-xs max-sm:!bg-opacity-75 max-sm:border-opacity-75 max-sm:!backdrop-blur w-full lg:p-4 p-2 max-sm:py-3"
   >
     <span
-      :class="{'bg-[length:100%_2px]': route.path === to}"
+      :class="{'bg-[length:100%_2px]': route.path === to && !hideActive}"
       class="bg-left-bottom bg-gradient-to-r from-gray-200 to-gray-200 group-hover:from-darkred group-hover:to-darkred bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-300 ease-out pb-0.5"
     >
       <slot />
